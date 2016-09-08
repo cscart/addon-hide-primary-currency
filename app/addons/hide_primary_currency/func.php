46a034aae9d160b234b4197c783fe637b7f25335
Roman Vlasenko <roof1rst@gmail.com>
2016-09-08 14:35:27 +0400

    Changed the text that users must replace with the currency code.

--- app/addons/hide_primary_currency/func.php
+++ app/addons/hide_primary_currency/func.php
@@ -6,7 +6,7 @@ if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 function fn_hide_primary_currency_init_currency_post(&$params, &$area, &$primary_currency, &$secondary_currency) {
     $primary_currency = 'DESIRED_ADMIN_CURRENCY_CODE';
-    $secondary_currency = 'DESIRED_ADMIN_CURRENCY_CODE';
+    $secondary_currency = 'DESIRED_STOREFRONT_CURRENCY_CODE';
 }
 
 function fn_hide_primary_currency_init_templater_post(&$view) {
