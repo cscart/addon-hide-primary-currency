de517b64f12d78ccc03dccf0bc7025343c6fdae3
Roman Vlasenko <roof1rst@gmail.com>
2016-09-21 11:18:33 +0400

    [~] {@17401} The curly brace is moved to the new line.

--- app/addons/hide_primary_currency/func.php
+++ app/addons/hide_primary_currency/func.php
@@ -4,12 +4,14 @@ use Tygh\Registry;
 
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
-function fn_hide_primary_currency_init_currency_post(&$params, &$area, &$primary_currency, &$secondary_currency) {
+function fn_hide_primary_currency_init_currency_post(&$params, &$area, &$primary_currency, &$secondary_currency)
+{
     $primary_currency = 'DESIRED_ADMIN_CURRENCY_CODE';
     $secondary_currency = 'DESIRED_STOREFRONT_CURRENCY_CODE';
 }
 
-function fn_hide_primary_currency_init_templater_post(&$view) {
+function fn_hide_primary_currency_init_templater_post(&$view)
+{
     $view->assign('hide_currencies', true);
     $view->assign('currencies', Registry::get('currencies'), false);
 }
