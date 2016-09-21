9ca3e265c44ca574a35d97b1baeeccebce41756f
Roman <admiralexo@gmail.com>
2016-09-21 17:28:27 +0400

    [~] {@17401} Variable moved in the module settings.

--- app/addons/hide_primary_currency/func.php
+++ app/addons/hide_primary_currency/func.php
@@ -6,8 +6,8 @@ if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 function fn_hide_primary_currency_init_currency_post(&$params, &$area, &$primary_currency, &$secondary_currency)
 {
-    $primary_currency = 'DESIRED_ADMIN_CURRENCY_CODE';
-    $secondary_currency = 'DESIRED_STOREFRONT_CURRENCY_CODE';
+    $primary_currency = Registry::get('addons.hide_primary_currency.admin_code');
+    $secondary_currency = Registry::get('addons.hide_primary_currency.storefront_code');
 }
 
 function fn_hide_primary_currency_init_templater_post(&$view)
