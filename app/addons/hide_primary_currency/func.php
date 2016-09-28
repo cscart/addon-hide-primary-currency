dab6f7534e107ebba0476259ece8fe2dd842a04d
Roman Vlasenko <roof1rst@gmail.com>
2016-09-28 11:30:39 +0400

    [~] {@17401} Added check user area.

--- app/addons/hide_primary_currency/func.php
+++ app/addons/hide_primary_currency/func.php
@@ -6,8 +6,11 @@ if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 function fn_hide_primary_currency_init_currency_post(&$params, &$area, &$primary_currency, &$secondary_currency)
 {
-    $primary_currency = Registry::get('addons.hide_primary_currency.admin_code');
-    $secondary_currency = Registry::get('addons.hide_primary_currency.storefront_code');
+    if ($area == 'A') {
+        $secondary_currency = Registry::get('addons.hide_primary_currency.admin_code');
+    } else {
+        $secondary_currency = Registry::get('addons.hide_primary_currency.storefront_code');
+    }
 }
 
 function fn_hide_primary_currency_init_templater_post(&$view)
