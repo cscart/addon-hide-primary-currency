7e7b52b533480a54301008832b2167ea203d4204
Roman Vlasenko <roof1rst@gmail.com>
2016-10-04 18:01:28 +0400

    [~] {@17401} The method is changed to another.

--- app/addons/hide_primary_currency/func.php
+++ app/addons/hide_primary_currency/func.php
@@ -7,7 +7,20 @@ if (!defined('BOOTSTRAP')) { die('Access denied'); }
 function fn_hide_primary_currency_init_currency_post(&$params, &$area, &$primary_currency, &$secondary_currency)
 {
     if ($area == 'A') {
-        $secondary_currency = Registry::get('addons.hide_primary_currency.admin_code');
+        $admin_code = Registry::get('addons.hide_primary_currency.admin_code');
+
+        if (array_key_exists('currency', $params)) {
+            $secondary_currency = Tygh::$app['session']['currency'] = $params['currency'];
+        } elseif (Tygh::$app['session']['currency']) {
+            $secondary_currency = Tygh::$app['session']['currency'];
+        } else {
+            $secondary_currency = $admin_code;
+        }
+
+        if ($params['dispatch'] == 'addons.update') {
+            $secondary_currency = $admin_code;
+            unset(Tygh::$app['session']['currency']);
+        }
     } else {
         $secondary_currency = Registry::get('addons.hide_primary_currency.storefront_code');
     }
